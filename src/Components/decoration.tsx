ac32465b5f3380f63b7a10bcbbacee8a94328ae7
Martin Sparstad <msparstad@gmail.com>
2024-01-07 12:47:42 +0100

    add sketch drawing to main page with import statement

--- src/Components/decoration.tsx
+++ src/Components/decoration.tsx
@@ -1,11 +1,14 @@
+import hero_img_url from "../assets/shaded_shapes.png"
+
 function Decoration() {
   return (
     <div className="decoration">
       <img
         className="image hero-image"
-        src="images/headphones.png"
+        src= {hero_img_url}
         alt="image of headphones with html tags between them"
       />
+
     </div>
   );
 }
