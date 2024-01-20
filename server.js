7d0391162ee7c12a375692b7259efe6ccb35cdfd
Martin Sparstad <msparstad@gmail.com>
2024-01-20 16:59:50 +0100

    move to basic node http

--- server.js
+++ server.js
@@ -1,13 +1,48 @@
 "use strict";
-export const __esModule = true;
-import { createServer } from "node:http";
-import * as fs from "node:fs";
+//export const __esModule = true;
+//import { createServer } from "node:http";
+//import * as fs from "node:fs";
+//import path from "node:path";
+
+const http = require("http");
+const fs =require("fs");
+const path = require("path")
+const src = require("src");
+
+
 
 //import main from "index.html";
 let entry_file = "";
+const base_path = __dirname;
+
+console.log(src);
+
+const mimeTypes = {
+    '.html': 'text/html',
+    '.jgp': 'image/jpeg',
+    '.css': 'text/css',
+    '.js': 'text/javascript',
+    '.tsx': 'text/javascript',
+    '.png': 'image/png',
+    '.ico': 'image/x-icon',
+    '.json': 'application/json',
+};
+
+const getContentType = (url) => {
+    let contentType = "application/octet-stream";
+    const extname = path.extname(url);
+    for (let key in mimeTypes) {
+        if (mimeTypes.hasOwnProperty(key)) {
+            if (extname === key) {
+                contentType = mimeTypes[key];
+            }
+        }
+    }
+    return contentType;
+}
 
 //console.log(entry_file);
-var server = createServer(function (req, res) {
+var server = http.createServer(function (req, res) {
 
     fs.readFile("./index.html", { encoding: "utf-8" }, (err, html) => { if (err) { throw err } else { entry_file = html; console.log(); } });
     //console.log(entry_file);
@@ -18,7 +53,7 @@ var server = createServer(function (req, res) {
     // }
 
     //entry_file.then(console.log(entry_file), console.log("promise failed"))
-    console.log(method + " " + url);
+    //console.log(method + " " + url);
     if (method === "GET" && url === "/") {
         console.log("caught a GET with url: " + url);
         res.writeHead(200, { 'Content-Type': 'text/html' })
@@ -27,6 +62,26 @@ var server = createServer(function (req, res) {
         res.end("bye");
     }
     else if (method === "GET") {
+        let true_path = path.join(base_path, url); 
+        console.log(true_path);
+        fs.access(true_path, (err) => {
+            console.log(err ? true_path + " doesn't exist " + err : true_path + " exists");
+            if (!err) {
+                fs.readFile(true_path, { encoding: "utf-8" }, (err, data) => {
+                    if (err) {
+                        //throw err
+                    }
+                    else {
+                        const contentType = getContentType(true_path);
+                        res.setHeader("Content-Type", contentType);
+
+                        res.write(data);
+                        console.log("wrote " + true_path + " to response");
+                    }
+                });
+            }
+            res.end;
+        })
 
     }
     else {
@@ -39,5 +94,5 @@ var server = createServer(function (req, res) {
 
 server.listen(8080);
 
-//closing server and exiting program after 15 seconds for testing
-setTimeout(() => { console.log("killing server"); server.close; process.exit(); }, 20000);
+//closing server and exiting program after specified milliseconds for testing
+//setTimeout(() => { console.log("killing server"); server.close; process.exit(); }, 20000);
